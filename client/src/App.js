61386a549caf615d60ea77047fce0a3a64e420a7
super dev <superdev1031@gmail.com>
2025-02-01 10:37:04 -0400

    add styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,45 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import usestyles from './styles';
+
+import instaverse from './images/Instaverse.png';
 
 const App = () => {
+  const classes = usestyles();
+
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Instaverse
+        </Typography>
+        <img
+          className="classes.image"
+          src={instaverse}
+          alt="instaverse"
+          height="60"
+        />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={4}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
