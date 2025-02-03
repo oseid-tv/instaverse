266fed9d4007672b72148c3d86cb5a91646f012b
super dev <superdev1031@gmail.com>
2025-02-03 10:11:54 -0400

    refine codes

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 
 const app = express();
+
+dotenv.config();
 
 app.use(bodyParser.json({ limit: '32mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '32mb', extended: true }));
@@ -13,12 +15,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL =
-  'mongodb+srv://superdev1031:GhxuHRtrQbAefhC6@cluster0.ujfzv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL)
+  .connect(process.env.CONNECTION_URL)
   .then(() =>
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
   )
